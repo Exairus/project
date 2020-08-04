787012753c767ae0f9ea185639aecad77f76bfe4
Will <example@gmail.com>
2020-08-04 11:31:35 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// console.log('arr' + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// decr--;
+
+// console.log(++incr);
+// console.log(decr);
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
